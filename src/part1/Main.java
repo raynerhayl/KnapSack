8402629cbf6fde377ad5e51246569fc9d017173b
raynerhayl <raynerhaylem@gmail.com>
2016-09-19 13:44:40 +1200

    Retrieves solution and Loads input from file
    Retrievs and prints the solution for the parcels to be included in the knapsack.
    
    Loads input from a file called parcels.txt in the res folder, format is just weight and value on each line for each parcel.

--- src/part1/Main.java
+++ src/part1/Main.java
@@ -2,37 +2,69 @@ package part1;
 
 import helpers.Parcel;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 public class Main {
 
     private int[][] dyArray;
 
-    public Main(){
-        List<Parcel> parcels = Arrays.asList(new Parcel[]{new Parcel(1,1), new Parcel(3,4), new Parcel(4,5),new Parcel(5,7)});
+    public Main() {
+        List<Parcel> parcels = loadParcels();//Arrays.asList(new Parcel[]{new Parcel(1, 1), new Parcel(3, 4), new Parcel(4, 5), new Parcel(5, 7)});
+
+        //loadParcels();
 
         Collections.sort(parcels);
 
         System.out.println("Parcel Array: ");
 
-        for(Parcel parcel: parcels){
+        for (Parcel parcel : parcels) {
             System.out.println(parcel);
         }
 
         System.out.println("\nOptimizing ... ");
 
-        initArray(parcels,7);
+        initArray(parcels, 18);
         optimize(parcels);
 
-        for(int i = 0; i < dyArray[0].length; i++){
-            for(int j = 0; j < dyArray.length; j++){
+        for (int i = 0; i < dyArray[0].length; i++) {
+            for (int j = 0; j < dyArray.length; j++) {
                 System.out.print(dyArray[j][i] + " ");
             }
             System.out.println("");
         }
 
+        System.out.println("\n");
+        System.out.println("Printing Solution: ");
+
+        getSolution(parcels);
+
+        for (int i = 0; i < parcels.size(); i++) {
+            System.out.println(parcels.get(i) + " " + parcels.get(i).isStatus());
+        }
+
+    }
+
+    public List<Parcel> loadParcels() {
+        List<Parcel> parcels = new ArrayList<Parcel>();
+
+        try {
+            File f = (new File("res/parcels.txt"));
+            if(f.exists()) {
+
+                Scanner scanner = new Scanner(f);
+                while (scanner.hasNext()) {
+                    parcels.add(new Parcel(scanner.nextInt(),scanner.nextInt()));
+                }
+            } else {
+                System.out.println("File doesn't exist");
+            }
+        } catch (IOException e) {
+
+        }
+
+        return parcels;
     }
 
     /**
@@ -42,7 +74,7 @@ public class Main {
      * @param parcelList a list of parcels sorted in increasing order by weight
      */
     private void initArray(List<Parcel> parcelList, int maxWeight) {
-        dyArray = new int[maxWeight+1][parcelList.size()];
+        dyArray = new int[maxWeight + 1][parcelList.size()];
     }
 
     private void optimize(List<Parcel> parcelList) {
@@ -61,23 +93,48 @@ public class Main {
                     if (subWeight <= subMaxWeight) {
                         subSolution = subValue;
                     }
-                } else if(subWeight > subMaxWeight) {
-                    subSolution = dyArray[subMaxWeight][subParcel-1];
-                } else{
+                } else if (subWeight > subMaxWeight) {
+                    subSolution = dyArray[subMaxWeight][subParcel - 1];
+                } else {
                     int previousSubSolution = dyArray[subMaxWeight][subParcel - 1]; // solution to problem if this parcel where not added
                     int nextLowestSubProblem = subMaxWeight - subWeight; // subProblem if this parcel where to be added
 
-                    System.out.println("subWeight: "+subWeight + " subMaxWeight: "+subMaxWeight);
-
                     int nextLowestSubSolution = subValue + dyArray[nextLowestSubProblem][subParcel - 1]; // solution to above suProblem
 
-                    subSolution = Math.max(previousSubSolution,nextLowestSubSolution);
+                    subSolution = Math.max(previousSubSolution, nextLowestSubSolution);
                 }
 
                 dyArray[subMaxWeight][subParcel] = subSolution;
 
             }
         }
+    }
+
+    /**
+     * Back tracks through the array of subProblems, constructing the
+     * solution of parcels given the same parcelList used to optimize
+     * the subProblems.
+     *
+     * @param parcelList the parcelList, sorted in increasing weight order
+     *                   used to optimize the subProblems.
+     */
+    private void getSolution(List<Parcel> parcelList) {
+        int solutionWeight = dyArray[dyArray.length - 1][dyArray[0].length - 1];
+        int subMaxWeight = dyArray.length - 1;
+        int subParcel = dyArray[0].length - 1;
+
+        while (subMaxWeight > 0) {
+            int previousSolution = dyArray[subMaxWeight][subParcel - 1]; // previous solution, without accepting the current subParcel
+            if (previousSolution == solutionWeight) {
+                parcelList.get(subParcel).setStatus(false); // parcel isnt in solution
+            } else {
+                parcelList.get(subParcel).setStatus(true); // parcel is in the solution
+                int previousSubMaxWeight = subMaxWeight - parcelList.get(subParcel).getWeight();
+                subMaxWeight = previousSubMaxWeight;
+            }
+            subParcel = subParcel - 1;
+        }
+
     }
 
     public static void main(String[] args) {
